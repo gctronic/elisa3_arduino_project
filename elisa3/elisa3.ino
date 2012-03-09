23a0adb1ab4351f83026a1abca1343fc78fc28b5
stefano <stefano@337712d9-d4f5-6d42-b37f-cd6ca520555a>
2012-03-09 07:03:58 +0000

    - arduino main project file name must be the same as the dir project name - main reduced to the minimum:   - eliminated uart sending code commented in the main   - selector not used anymore: default behavior is calling the speed controller - use "setup" and "loop" standard arduino project functions (instead of main)
    git-svn-id: file:///P/lavoro/local-repo/elisaIII/arduino/Elisa3-arduino@54 337712d9-d4f5-6d42-b37f-cd6ca520555a

---
+++ elisa3/elisa3.ino
@@ -0,0 +1,50 @@
+#include <adc.h>
+#include <behaviors.h>
+#include <constants.h>
+#include <ir_remote_control.h>
+#include <leds.h>
+#include <mirf.h>
+#include <motors.h>
+#include <nRF24L01.h>
+#include <ports_io.h>
+#include <sensors.h>
+#include <speed_control.h>
+#include <spi.h>
+#include <twimaster.h>
+#include <usart.h>
+#include <utility.h>
+#include <variables.h>
+
+unsigned long int startTime = 0, endTime = 0;
+
+void setup() {
+
+  initPeripherals();
+  calibrateSensors();
+  initBehaviors();
+  startTime = getTime100MicroSec();  
+
+}
+
+void loop() {
+
+  currentSelector = getSelector();	// update selector position
+
+  readAccelXYZ();						// update accelerometer values to compute the angle
+
+  computeAngle();
+	
+  endTime = getTime100MicroSec();
+  if((endTime-startTime) >= (PAUSE_2_SEC)) {
+    readBatteryLevel();				// the battery level is updated every two seconds
+    startTime = getTime100MicroSec();
+  }
+
+  handleIRRemoteCommands();
+
+  handleRFCommands();
+
+  handleMotorsWithSpeedController();  
+  
+}
+
